add1d65ba84e5304250139797580b0b06193bf41
ankit <ankit113912@gmail.com>
2026-08-22 04:33:29 -0700

    simple hosting idea

---
+++ 17th_Deployment.py
@@ -0,0 +1,23 @@
+# simple self-Hosted  Deployment (suru karne ke liye )
+
+"""
+# LangGraph CLI install karo
+pip install langgraph-cli
+
+# Apne project mein ek config file banao: langgraph.json
+
+{
+  "dependencies": ["."],
+  "graphs": {
+    "my_agent": "./my_agent.py:app"
+  },
+  "env": ".env"
+}
+
+# Local server chalao (testing ke liye)
+langgraph dev
+
+langgraph dev
+# Fir browser mein LangGraph Studio khol ke apne local server se connect karo
+
+"""
